a8a59d114daa8f07036e70c952f757ae3d5f9424
Kumonorei <scott.macleod9@gmail.com>
2015-04-05 22:32:54 +0100

    Further whitespace correction

--- lothariel.py
+++ lothariel.py
@@ -1,3 +1,5 @@
+#!python2
+
 ######################
 #                    #
 # LANDS OF LOTHARIEL #
@@ -56,20 +58,20 @@ class Tile:
 class Rect:
     # a rectangle on the map, for making a room
     def __init__(self, x, y, w, h):
-    self.x1 = x
-    self.y1 = y
-    self.x2 = x + w
-    self.y2 = y + h
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
     
     def center(self):
-    center_x = (self.x1 + self.x2) / 2
-    center_y = (self.y1 + self.y2) / 2
-    return (center_x, center_y)
+        center_x = (self.x1 + self.x2) / 2
+        center_y = (self.y1 + self.y2) / 2
+        return (center_x, center_y)
 
     def intersect(self, other):
-    #returns true if this rectangle intersects with another one
-    return (self.x1 <= other.x2 and self.x2 >= other.x1 and
-        self.y1 <= other.y2 and self.y2 >= other.y1)
+        #returns true if this rectangle intersects with another one
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+            self.y1 <= other.y2 and self.y2 >= other.y1)
     
 # object setup
 class Object:
@@ -88,9 +90,9 @@ class Object:
     
     # set the color and draw the character representing the object on screen
     def draw(self):
-    if libtcod.map_is_in_fov(fov_map, self.x, self.y):
-        libtcod.console_set_default_foreground(con, self.color)
-        libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+        if libtcod.map_is_in_fov(fov_map, self.x, self.y):
+            libtcod.console_set_default_foreground(con, self.color)
+            libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
         
     # erase the character representing this object
     def clear(self):
@@ -101,22 +103,22 @@ def create_room(room):
     global map
     # go through the tiles in a rectangle and make them passable
     for x in range(room.x1 + 1, room.x2):
-    for y in range(room.y1 + 1, room.y2):
-        map[x][y].blocked = False
-        map[x][y].block_sight = False
+        for y in range(room.y1 + 1, room.y2):
+            map[x][y].blocked = False
+            map[x][y].block_sight = False
         
 # tunnel builder
 def create_h_tunnel(x1, x2, y):
     global map
     for x in range(min(x1, x2), max(x1, x2) +1):
-    map[x][y].blocked = False
-    map[x][y].block_sight = False
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
     
 def create_v_tunnel(y1, y2, x):
     global map
     for y in range(min(y1, y2), max(y1, y2) +1):
-    map[x][y].blocked = False
-    map[x][y].block_sight = False
+        map[x][y].blocked = False
+        map[x][y].block_sight = False
         
 def make_map():
     global map
@@ -130,52 +132,52 @@ def make_map():
     num_rooms = 0
     
     for r in range(MAX_ROOMS):
-    # random width and height
-    w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-    h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
-    # random position without leaving boundary of map
-    x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
-    y = libtcod.random_get_int(0, 0, MAP_HEIGHT - w - 1)
+        # random width and height
+        w = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        h = libtcod.random_get_int(0, ROOM_MIN_SIZE, ROOM_MAX_SIZE)
+        # random position without leaving boundary of map
+        x = libtcod.random_get_int(0, 0, MAP_WIDTH - w - 1)
+        y = libtcod.random_get_int(0, 0, MAP_HEIGHT - w - 1)
     
-    new_room = Rect(x, y, w, h)
+        new_room = Rect(x, y, w, h)
     
-    #run through other rooms to check for overlaps
-    failed = False
-    for other_room in rooms:
-        if new_room.intersect(other_room):
-        failed = True
-        break
-    if not failed:
-        # draw the room
-        create_room(new_room)
+        #run through other rooms to check for overlaps
+        failed = False
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                failed = True
+                break
+            if not failed:
+                # draw the room
+                create_room(new_room)
         
-        # get the center coordinates
-        (new_x, new_y) = new_room.center()
+                # get the center coordinates
+                (new_x, new_y) = new_room.center()
         
-        if num_rooms == 0:
-        # set starting point in the first room
-        player.x = new_x
-        player.y = new_y
+                if num_rooms == 0:
+                    # set starting point in the first room
+                    player.x = new_x
+                    player.y = new_y
           
-        else:
-        # for all rooms after the first, make a tunnel from the previous room
-        # using the center coordinates of the previous room
-        (prev_x, prev_y) = rooms[num_rooms-1].center()
+                else:
+                    # for all rooms after the first, make a tunnel from the previous room
+                    # using the center coordinates of the previous room
+                    (prev_x, prev_y) = rooms[num_rooms-1].center()
         
-        # flip a coin
-        if libtcod.random_get_int(0, 0, 1) == 1:
-            # horizontal first, vertical second
-            create_h_tunnel(prev_x, new_x, prev_y)
-            create_v_tunnel(prev_y, new_y, new_x)
+                    # flip a coin
+                    if libtcod.random_get_int(0, 0, 1) == 1:
+                        # horizontal first, vertical second
+                        create_h_tunnel(prev_x, new_x, prev_y)
+                        create_v_tunnel(prev_y, new_y, new_x)
+                
+                    else:
+                        # vertical first, horizontal second
+                        create_v_tunnel(prev_y, new_y, prev_x)
+                        create_h_tunnel(prev_x, new_x, new_y)
             
-        else:
-            # vertical first, horizontal second
-            create_v_tunnel(prev_y, new_y, prev_x)
-            create_h_tunnel(prev_x, new_x, new_y)
-            
-        # append the new room to the list
-        rooms.append(new_room)
-        num_rooms += 1
+                # append the new room to the list
+                rooms.append(new_room)
+                num_rooms += 1
         
       
 # render function
@@ -187,25 +189,25 @@ def render_all():
        
     # fov 
     if fov_recompute:
-    # recompute fov if required
-    fov_recompute = False
-    libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+        # recompute fov if required
+        fov_recompute = False
+        libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
         
     # draw the map
     for y in range(MAP_HEIGHT):
         for x in range(MAP_WIDTH):
-        visible = libtcod.map_is_in_fov(fov_map, x, y)
+            visible = libtcod.map_is_in_fov(fov_map, x, y)
             wall = map[x][y].block_sight
             if not visible:
-        if wall:
-            libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
-        else:
-            libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
+                if wall:
+                    libtcod.console_set_char_background(con, x, y, color_dark_wall, libtcod.BKGND_SET)
+                else:
+                    libtcod.console_set_char_background(con, x, y, color_dark_ground, libtcod.BKGND_SET)
             else:
-        if wall:
-            libtcod.console_set_char_background(con, x, y, color_light_wall,libtcod.BKGND_SET)
-        else:
-            libtcod.console_set_char_background(con, x, y, color_light_ground, libtcod.BKGND_SET)
+                if wall:
+                    libtcod.console_set_char_background(con, x, y, color_light_wall,libtcod.BKGND_SET)
+                else:
+                    libtcod.console_set_char_background(con, x, y, color_light_ground, libtcod.BKGND_SET)
     
     # draw all objects in the object list 
     for object in objects:
@@ -282,7 +284,7 @@ make_map()
 fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
 for y in range(MAP_HEIGHT):
     for x in range(MAP_WIDTH):
-     libtcod.map_set_properties(fov_map, x, y, not map[x][y].block_sight, not map[x][y].blocked)
+        libtcod.map_set_properties(fov_map, x, y, not map[x][y].block_sight, not map[x][y].blocked)
 
 
 fov_recompute = True
